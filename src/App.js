dea842b7889a47b355a0fbc2af039bbc98412c7b
SinChaNo <atom7667@naver.com>
2022-09-24 15:44:48 +0900

    LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import IterationSample from './components/IterationSample';
+import LifeCycleSample from './components/LIfeCycleSample';
 
 
 class App extends Component {
   render() {
     return(
       <div>
-        <IterationSample />
+        <LifeCycleSample />
       </div>
     );
   }
